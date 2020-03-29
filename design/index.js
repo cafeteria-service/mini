4bbd0e6bd422de01dd6cb2da538b373a16b70b95
NeetaBiradar <36724953+NeetaBiradar@users.noreply.github.com>
2020-03-29 11:44:50 +0530

    Add files via upload

---
+++ design/index.js
@@ -0,0 +1,22 @@
+
+  function login(){
+  
+    var userEmail = document.getElementById("exampleInputEmail1" ).value;
+    var userPass = document.getElementById("exampleInputPassword1").value;
+  
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).then(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+  
+      window.alert("Error : " + errorMessage);
+  
+      // ...
+    });
+  
+  }
+
+  function logout(){
+    firebase.auth().signOut();
+  }
+  
